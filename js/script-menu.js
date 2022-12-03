dcfcc4cde6bb03ddd9ac4a4077f9be142e7f689e
AndrMar1939 <advo737@gmail.com>
2022-12-03 15:39:05 +0200

    adaptive and responsible site, ready to use

---
+++ js/script-menu.js
@@ -0,0 +1,19 @@
+
+let menuBtn = document.querySelector(".menu-btn");
+let navBar = document.querySelector(".nav-bar");
+let logoAdaptive = document.querySelector(".logo-adaptive");
+let navItem = document.querySelector(".nav-item");
+
+
+
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("active");
+  navBar.classList.toggle("active");
+  logoAdaptive.classList.toggle("active");
+});
+
+navItem.addEventListener("click", function () {
+  menuBtn.classList.remove("active");
+  navBar.classList.remove("active");
+  logoAdaptive.classList.remove("active");
+});
